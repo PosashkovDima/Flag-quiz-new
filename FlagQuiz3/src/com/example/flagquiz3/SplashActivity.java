4d73eb17b942ac948982116d673fb88d6f853462
Dima Posashkov <k0kak0la1488@gmail.com>
2014-05-12 10:56:46 +0400

    Fix splash screen

--- FlagQuiz3/src/com/example/flagquiz3/SplashActivity.java
+++ FlagQuiz3/src/com/example/flagquiz3/SplashActivity.java
@@ -1,13 +1,48 @@
 package com.example.flagquiz3;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 public class SplashActivity extends Activity {
+	protected int SPLASH_SCREEN_DELAY = 1000;
+
+	private Thread splashTread;
+
+	/** Called when the activity is first created. */
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+		setContentView(R.layout.a_splash_screen);
+
+		splashTread = new Thread() {
+			@Override
+			public void run() {
+				try {
+					synchronized (this) {
+						wait(SPLASH_SCREEN_DELAY);
+					}
+				} catch (InterruptedException e) {
+				} finally {
+					finish();
+					Intent intent = new Intent(SplashActivity.this,
+							GameScreen.class);
+					startActivity(intent);
+				}
+			}
+		};
+		splashTread.start();
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		if (event.getAction() == MotionEvent.ACTION_DOWN) {
+			synchronized (splashTread) {
+				splashTread.notifyAll();
+			}
+		}
+		return true;
 	}
 
 }
